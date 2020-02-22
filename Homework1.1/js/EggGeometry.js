2b2e3c4d1546611308a8478646b8603729877b80
Eliana Neurohr <elianajneu@gmail.com>
2020-02-22 14:49:23 +0100

    egg working

---
+++ Homework1.1/js/EggGeometry.js
@@ -0,0 +1,107 @@
+"use strict";
+/* exported TriangleGeometry */
+class EggGeometry {
+  constructor(gl) {
+    this.gl = gl;
+
+    // allocate and fill vertex buffer in device memory (OpenGL name: array buffer)
+    this.vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
+    this.vertex_array = new Float32Array(3*11);
+    var R = 0.5;
+    var r = 0.2;
+    var l = 0.0015;
+    var a = 0.25;
+    var b = 0.1;
+
+    var alpha = 5 * (Math.PI/180);
+    this.vertex_array = new Float32Array(3*72);
+    this.vertex_array[0] = 0;
+    this.vertex_array[1] = 0;
+    this.vertex_array[2] = 0.5;
+    var counter = 3;
+    var small_rad = true;
+    var rad = this.r;
+    for(var theta = (Math.PI/180); theta < (2*Math.PI); theta += alpha)
+    {
+      this.vertex_array[counter] = l*Math.cos(theta) + (a + b*Math.cos(theta))*Math.cos(theta);
+      counter++;
+      this.vertex_array[counter] = (a + b*Math.cos(theta))*Math.sin(theta);
+      counter++;
+      this.vertex_array[counter] = 0.5;
+      counter++;
+    }
+
+    gl.bufferData(gl.ARRAY_BUFFER,
+      this.vertex_array,
+      gl.STATIC_DRAW);
+
+
+    //hw:add new vertexbuffer for color
+    this.vertexColor = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexColor);
+    this.color_array = new Float32Array(3*72);
+    this.color_array[0] = 0.824;
+    this.color_array[1] = 0.706;
+    this.color_array[2] = 0.549;
+    for (var i = 3; i < (3*72); i++){
+      if(i%3 === 0)
+        this.color_array[i] = 0.569;
+      if(i%3 === 1)
+        this.color_array[i] = 0.506;
+      if(i%3 === 2)
+        this.color_array[i] = 0.318;
+    }
+       
+    gl.bufferData(gl.ARRAY_BUFFER,
+        this.color_array,
+        gl.STATIC_DRAW);
+
+        // allocate and fill index buffer in device memory (OpenGL name: element array buffer)
+        this.indexBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+        this.unit_array = new Uint16Array(73);
+        for (var i = 0; i < 73; i++){
+          this.unit_array[i] = i;
+          if (i == 72) { this.unit_array[i] = 1; }
+        }
+        console.log(this.unit_array);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,
+          this.unit_array,
+          gl.STATIC_DRAW);
+
+    // create and bind input layout with input buffer bindings (OpenGL name: vertex array)
+    this.inputLayout = gl.createVertexArray();
+    gl.bindVertexArray(this.inputLayout);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
+    gl.enableVertexAttribArray(0);
+    gl.vertexAttribPointer(0,
+      3, gl.FLOAT, //< three pieces of float
+      false, //< do not normalize (make unit length)
+      0, //< tightly packed
+      0 //< data starts at array start
+    );
+
+    //hw: binding enabling new vertexColor above. This below is the "input layout specification"
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexColor);
+    gl.enableVertexAttribArray(1);
+    gl.vertexAttribPointer(1,
+      3, gl.FLOAT, //< three pieces of float
+      false, //< do not normalize (make unit length)
+      0, //< tightly packed
+      0 //< data starts at array start
+    );
+
+    gl.bindVertexArray(null);
+  }
+
+  draw() {
+    const gl = this.gl;
+
+    gl.bindVertexArray(this.inputLayout);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+
+    gl.drawElements(gl.TRIANGLE_FAN, 73, gl.UNSIGNED_SHORT, 0); //gl.TRIANGLES, gl.TRIANGLE_FAN
+  }
+}
--- Homework1.1/js/Scene.js
+++ Homework1.1/js/Scene.js
@@ -11,7 +11,9 @@ class Scene {
     // this.heartGeometry = new HeartGeometry(gl);
     // this.crescentGeometry = new CrescentGeometry(gl);
     this.donutGeometry = new DonutGeometry(gl);
+    this.eggGeometry = new EggGeometry(gl);
     this.avatar_position = {x:0, y:0, z:0};
+    this.eggUniform = {x:-0.7, y:0.5, z:0};
   }
 
   resize(gl, canvas) {
@@ -32,19 +34,22 @@ class Scene {
     // if(keysPressed.UP){this.avatar_position.y += 0.01;}
     // if(keysPressed.DOWN){this.avatar_position.y -= 0.01;}
 
-
     gl.useProgram(this.solidProgram.glProgram);
     // this.triangleGeometry.draw();
     // this.quadGeometry.draw();
-    this.donutGeometry.draw();
+    // this.donutGeometry.draw(); //move this
+    // gl.uniform(vec4, this.eggUniform.x, this.eggUniform.y, this.eggUniform.z);
     // this.heartGeometry.draw();
 
     const objectPositionHandle = gl.getUniformLocation(this.solidProgram.glProgram, "gameObject.position");
     if (objectPositionHandle == null){
       console.log("could not find uniform: gameObject.position");
     } else {
-      gl.uniform3f(objectPositionHandle, this.avatar_position.x, this.avatar_position.y, this.avatar_position.z);
+      gl.uniform3f(objectPositionHandle, this.eggUniform.x, this.eggUniform.y, this.eggUniform.z);
     }
+    this.eggGeometry.draw();
+    gl.uniform3f(objectPositionHandle, 0.0, 0.0, 0.0);
+    this.eggGeometry.draw();
     // this.triangleGeometry.draw();
   }
 }
