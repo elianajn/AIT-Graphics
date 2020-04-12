932644dbcad92ab0c7bdf82e0fb6c3495c57947b
Eliana Neurohr <elianajneu@gmail.com>
2020-04-11 23:34:27 -0500

    flames workinggit add *

--- Homework 2D Final/js/Scene.js
+++ Homework 2D Final/js/Scene.js
@@ -31,7 +31,18 @@ class Scene extends UniformProvider {
     this.flames.position.set(-1.08,0.3);
     this.flames.orientation = Math.PI - 0.2;
     this.flames.scale = (0.7,0.7,0.7);
-    this.gameObjects.push(this.flames);
+
+    this.flamesRight = new GameObject(this.flamesMesh);
+    this.flamesRight.position.set(-0.75,-0.75);
+    this.flamesRight.orientation = 5*Math.PI/4 - 0.3;
+    this.flamesRight.scale = (0.4,0.4,0.4);
+    // this.gameObjects.push(this.flamesRight);
+
+    this.flamesLeft = new GameObject(this.flamesMesh);
+    this.flamesLeft.position.set(-0.75,1.0);
+    this.flamesLeft.orientation = 3*Math.PI/4 + 0.3;
+    this.flamesLeft.scale = (0.4,0.4,0.4);
+    // this.gameObjects.push(this.flamesLeft);
 
     this.raiderMaterial = new Material(this.texturedProgram);
     this.raiderMaterial.colorTexture.set(new Texture2D(gl, "media/raider.png"));
@@ -65,28 +76,76 @@ class Scene extends UniformProvider {
       asteroid.move = genericMove;
     }
 
+    const remove = function(ary, elem) {
+      var i = ary.indexOf(elem);
+      if (i >= 0) ary.splice(i, 1);
+      return ary;
+    };
+
 
     this.avatar.backDrag = 0.9;
     this.avatar.sideDrag = 0.5;
     this.avatar.angularDrag = 0.5;
     // this.avatar.aheadVector = new Vec3();
     this.avatar.thrust = 0;
+    var gameObjects = this.gameObjects;
+    var flames = this.flames;
+    var flamesRight = this.flamesRight;
+    var flamesLeft = this.flamesLeft;
     this.avatar.control = function(t, dt, keysPressed, colliders){
       this.thrust = 0;
       this.torque = 0;
-      if(keysPressed.LEFT){this.torque += 1;}
-      if(keysPressed.RIGHT){this.torque -= 1;}
+      // gameObjects.splice()
+      remove(gameObjects, flames);
+      remove(gameObjects, flamesRight);
+      remove(gameObjects, flamesLeft);
+      if(keysPressed.LEFT){
+        this.torque += 1;
+        gameObjects.push(flamesRight);
+      }
+      if(keysPressed.RIGHT){
+        this.torque -= 1;
+        gameObjects.push(flamesLeft);
+      }
       if(keysPressed.UP){
         this.thrust += 1;
+        gameObjects.push(flames);
       }
       if(keysPressed.DOWN){
         this.thrust -= 1;
+        gameObjects.push(flamesRight);
+        gameObjects.push(flamesLeft);
       }
     };
-    const theta = this.avatar.orientation;
-    const avatarMove = function(t,dt){
-      this.aheadVector = new Vec3(Math.cos(theta), Math.sin(theta),0);
-      this.force = (this.aheadVector).mul(this.thrust); // avatar does not move if the up or down key is not being held
+    // this.theta = this.avatar.orientation;
+    // const avatarMove = function(t,dt){
+    //   this.aheadVector = new Vec3(Math.cos(theta), Math.sin(theta),0);
+    //   this.force = (this.aheadVector).mul(this.thrust); // avatar does not move if the up or down key is not being held
+    //   const acceleration = new Vec3(this.force).mul(this.invMass);
+    //   const momentum = 0.0;
+    //   const initial_velocity = momentum * this.invMass;
+    //   this.velocity.addScaled(dt, initial_velocity);
+    //   this.velocity.addScaled(dt, acceleration);
+    //   this.position.addScaled(dt, this.velocity);
+      // this.velocity.mul(Math.exp(-dt * c * this.invMass));
+
+    //   const angularAcceleration = this.torque * this.invAngularMass;
+    //   this.angularVelocity += angularAcceleration * dt;
+    //   this.orientation += this.angularVelocity * dt;
+    // };
+    // console.log(this.avatar.force);
+    this.avatar.invMass = 0.5;
+    // this.flames.invMass = 0.5;
+    // this.avatar.move = avatarMove;
+    this.flames.parent = this.avatar;
+    this.flamesRight.parent = this.avatar;
+    this.flamesLeft.parent = this.avatar;
+    // this.flames.move = avatarMove;
+    // this.flames.control = this.avatar.control;
+
+    this.avatar.move = function(t, dt) {
+      this.aheadVector = new Vec3(Math.cos(this.orientation), Math.sin(this.orientation),0);
+      this.force = new Vec3(this.aheadVector).mul(this.thrust); // avatar does not move if the up or down key is not being held
       const acceleration = new Vec3(this.force).mul(this.invMass);
       const momentum = 0.0;
       const initial_velocity = momentum * this.invMass;
@@ -99,29 +158,6 @@ class Scene extends UniformProvider {
       this.angularVelocity += angularAcceleration * dt;
       this.orientation += this.angularVelocity * dt;
     };
-    // console.log(this.avatar.force);
-    this.avatar.invMass = 0.5;
-    // this.flames.invMass = 0.5;
-    this.avatar.move = avatarMove;
-    this.flames.parent = this.avatar;
-    // this.flames.move = avatarMove;
-    // this.flames.control = this.avatar.control;
-
-    // this.avatar.move = function(t, dt) {
-      // this.aheadVector = new Vec3(Math.cos(this.orientation), Math.sin(this.orientation),0);
-      // this.force = new Vec3(this.aheadVector).mul(this.thrust); // avatar does not move if the up or down key is not being held
-      // const acceleration = new Vec3(this.force).mul(this.invMass);
-      // const momentum = 0.0;
-      // const initial_velocity = momentum * this.invMass;
-      // this.velocity.addScaled(dt, initial_velocity);
-      // this.velocity.addScaled(dt, acceleration);
-      // this.position.addScaled(dt, this.velocity);
-      // // this.velocity.mul(Math.exp(-dt * c * this.invMass));
-      //
-      // const angularAcceleration = this.torque * this.invAngularMass;
-      // this.angularVelocity += angularAcceleration * dt;
-      // this.orientation += this.angularVelocity * dt;
-    // };
     // this.flames.control = this.avatar.control;
 
     this.timeAtFirstFrame = new Date().getTime();
@@ -152,15 +188,26 @@ class Scene extends UniformProvider {
     this.camera.position = this.avatar.position;
     this.camera.update();
 
+    const theta = this.avatar.orientation;
+
     if(keysPressed.SPACE){
       // this.plasma = new GameObject(this.plasmaMesh);
       // this.gameObjects.push.(this.plasma);
       this.plasma = new GameObject(this.plasmaMesh);
-      this.plasma.position.set(-13, -13);
+      this.plasma.position.set(this.avatar.position);
       this.plasma.scale.set(0.3,0.3,0.3);
       console.log(this.plasma);
       console.log(this.gameObjects);
-      // this.plasma.move =
+      this.plasma.move = function(t, dt){
+        this.aheadVector = new Vec3(Math.cos(theta), Math.sin(theta),0);
+        this.force = new Vec3(this.aheadVector).mul(20.0);
+        const acceleration = new Vec3(this.force).mul(this.invMass);
+        // const momentum = 0.0;
+        // const initial_velocity = momentum * this.invMass;
+        // this.velocity.addScaled(dt, initial_velocity);
+        this.velocity.addScaled(dt, acceleration);
+        this.position.addScaled(dt, this.velocity);
+      };
       this.gameObjects.push(this.plasma);
     }
 
